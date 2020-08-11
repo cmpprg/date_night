195266bed8dcea2976d79b610e467d4dd643e9a0
cmpprg <campryan@comcast.net>
2020-08-11 10:04:14 -0600

    Write test to check attributes of tree node

--- tests/node_test.rb
+++ tests/node_test.rb
@@ -3,9 +3,18 @@ require 'minitest/pride'
 require './lib/node'
 
 class NodeTest < Minitest::Test
-  def test_that_it_exists
-    node = TreeNode.new(44, 'Ryan\'s Revenge')
+  def setup
+    @node = TreeNode.new(44, 'Ryan\'s Revenge')
+  end
 
-    assert_instance_of TreeNode, node
+  def test_that_it_exists
+    assert_instance_of TreeNode, @node
+  end
+
+  def test_it_has_attributes
+    assert_equal 44, @node.movie_score
+    assert_equal 'Ryan\'s Revenge', @node.movie_title
+    assert_equal nil, @node.left
+    assert_equal nil, @node.right
   end
 end
