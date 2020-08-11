f040baa84cb6c465684ad4bc602be505ebae9747
cmpprg <campryan@comcast.net>
2020-08-11 10:25:42 -0600

    Add attributes and accessors for left and right node

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,8 @@
 class TreeNode
+  attr_reader :movie_score, :movie_title
+  attr_accessor :left, :right
   def initialize( score, title)
-
+    @movie_score = score
+    @movie_title = title
   end
 end
