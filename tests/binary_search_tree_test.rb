7d747e127c6c2c46380c4c9d2f0d2beb1c58f57e
cmpprg <campryan@comcast.net>
2020-08-11 12:07:45 -0600

    Write tests, set root with tree node, set other nodes after, and return depth

--- tests/binary_search_tree_test.rb
+++ tests/binary_search_tree_test.rb
@@ -20,4 +20,42 @@ class BinarySearchTreeTest < Minitest::Test
 
     assert_equal 'root', @tree.root
   end
+
+  def test_it_can_insert_a_node_into_root
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    root_node = @tree.root
+
+    assert_instance_of TreeNode, root_node
+    assert_equal 61, root_node.movie_score
+    assert_equal "Bill & Ted's Excellent Adventure", root_node.movie_title
+  end
+
+  def test_after_root_is_set_it_can_other_nodes_to_left_and_right
+    skip
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    root_node = @tree.root
+
+    @tree.insert(16, "Johnny English")
+
+    assert_equal "Johnny English", root_node.left.movie_title
+    assert_equal 16, root_node.left.movie_score
+
+    @tree.insert(92, "Sharknado 3")
+
+    assert_equal "Sharknado 3", root_node.right.movie_title
+    assert_equal 92, root_node.right.movie_score
+
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal "Hannibal Buress: Animal Furnace", root_node.left.right.movie_title
+    assert_equal 50, root_node.left.right.movie_score
+  end
+
+  def test_it_can_return_the_depth_of_node_after_insertion
+    skip
+    assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, @tree.insert(16, "Johnny English")
+    assert_equal 1, @tree.insert(92, "Sharknado 3")
+    assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
+  end
 end
