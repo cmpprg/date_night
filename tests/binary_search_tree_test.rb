63f97e6050a85a09e571cef61a5df4f16333ffb2
cmpprg <campryan@comcast.net>
2020-08-11 11:26:57 -0600

    Write test to ensure root can be set after instantiation

--- tests/binary_search_tree_test.rb
+++ tests/binary_search_tree_test.rb
@@ -14,4 +14,10 @@ class BinarySearchTreeTest < Minitest::Test
   def test_it_has_attribute
     assert_nil @tree.root
   end
+
+  def test_root_attribute_can_be_set_after_instantiation
+    @tree.root = 'root'
+
+    assert_equal 'root', @tree.root
+  end
 end
