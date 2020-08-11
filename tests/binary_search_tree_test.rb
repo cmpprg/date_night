09db89bc4c4ee42b404f6ef8f6110721597118b6
cmpprg <campryan@comcast.net>
2020-08-11 11:12:15 -0600

    Write existence test for binary search tree

--- tests/binary_search_tree_test.rb
+++ tests/binary_search_tree_test.rb
@@ -2,6 +2,12 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/binary_search_tree'
 
-class BinarySearchTree < Minitest::Test
-  
+class BinarySearchTreeTest < Minitest::Test
+  def setup
+    @tree = BinarySearchTree.new
+  end
+
+  def test_that_it_exists
+    assert_instance_of BinarySearchTree, tree
+  end
 end
