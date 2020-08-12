ad0e2f7083088035795a2bb5b05abdc23c7e544f
cmpprg <campryan@comcast.net>
2020-08-12 08:58:19 -0600

    Write test to check depth_of method

--- tests/binary_search_tree_test.rb
+++ tests/binary_search_tree_test.rb
@@ -66,4 +66,15 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal true, @tree.include?(50)
     assert_equal false, @tree.include?(72)
   end
+
+  def test_it_can_display_depth_of_a_score
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(92, "Sharknado 3")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal 1, @tree.depth_of(92)
+    assert_equal 2, @tree.depth_of(50)
+    assert_nil @tree.depth_of(72)
+  end
 end
